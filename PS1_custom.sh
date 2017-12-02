7ad537e3a6aaf37bf3d04fb8f07f665865515cd5
Ari Sweedler <AriSweedler@users.noreply.github.com>
2017-12-02 04:53:11 -0800

    Changed the scheme to remove boilerplate commands

--- PS1_custom.sh
+++ PS1_custom.sh
@@ -1,8 +1,10 @@
-#This script makes it easier to create a colored prompt script.
+#!/bin/bash
 
-#'non-printing escape sequences have to be enclosed in \[ESC[ and \].'
-NO_COLOR='\[\e[0m\]'  #\033=\e\=^[=\0x1b=ECH in ascii.
-_SC=NO_COLOR
+#This script makes it easier to create a colored prompt script.
+#Bash colored output works by sending an unprinted escape sequence to get the console to switch "pen color"
+#non-printing escape sequences have to be enclosed in \[ESC[ and \].
+NO_COL='\[\e[0m\]'  #'\033'='\e'='^['='\0x1b'='ESC in ascii'
+SET_COL=$NO_COL
 
 #Function to set color
 function SET_C() {
@@ -23,7 +25,7 @@ function SET_C() {
         "italic")       STYLE='03';;
         "underline")    STYLE='04';;
         "reverse")      STYLE='07';;
-        *)              _SC=NO_COLOR; echo "error in style: '$1'"; return;;
+        *)              SET_COL=NO_COL; echo "error in style: '$1'"; return;;
     esac
 
     case $2 in
@@ -35,23 +37,25 @@ function SET_C() {
         "PURPLE")       COLOR="35";;
         "CYAN")         COLOR="36";;
         "WHITE")        COLOR="37";;
-        *)              _SC=NO_COLOR; echo "error in color: '$2'"; return;;
+        *)              SET_COL=NO_COL; echo "error in color: '$2'"; return;;
     esac
     # Start color variable
-    _SC="\[\e[${STYLE};${COLOR}m\]"
+    SET_COL="\[\e[${STYLE};${COLOR}m\]"
     #Fun fact: we have to escape the brackets to make sure that bash knows how to calculate PS1's length.
     #Without the escaped brackets, bash would miscalculate PS1's length and wrap around at the wrong time.
 }
 
 #This function makes my styling calls prettier.
-#Usage: STYLE <RESULT> <'string'> [[<style>] <color>]
+#Usage: STYLE <VAR> <'string'> [[<style>] <color>]
 function STYLE() {
   SET_C $3 $4
-  eval "$1='${_SC}$2'" #This establishes $1 as a shell variable.
+  eval "$1='${SET_COL}$2${NO_COL}'" #This establishes $1 as a shell variable.
+  #I place NO_COL at the end because having `plain` style doesn't overwrite.
+  #If you underline the first chunk then leeave the rest as plain, everything will come out as underlined.
 }
 
 ################################
-# Set the colors and parts of the prompt.
+# Set the colors and parts of the prompt. Below this is the customizable part. Get creative!
 #Check out https://www.cyberciti.biz/tips/howto-linux-unix-bash-shell-setup-prompt.html as a reference for special PS1 chars
 STYLE PS1_TIME '[\@]' dark YELLOW
 STYLE PS1_USER '\u' underline RED
@@ -59,7 +63,7 @@ STYLE PS1_AT '@' light BLUE
 STYLE PS1_HOST '\h' light RED
 STYLE PS1_DIR '\W' dark GREEN
 STYLE PS1_END '> ' light BLUE
-STYLE OPENB '[' dark GREEN; STYLE CLOSEB
+STYLE OPENB '[' dark GREEN; STYLE CLOSEB ']'
 
 ################################
 # Do the actual export, placing the parts in any order
