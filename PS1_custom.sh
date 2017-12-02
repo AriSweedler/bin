65623ecfdd16d9975f24025f5b89b3615fda4d55
Ari Sweedler <AriSweedler@users.noreply.github.com>
2017-12-02 04:20:57 -0800

    Update PS1_custom.sh

--- PS1_custom.sh
+++ PS1_custom.sh
@@ -1,5 +1,15 @@
-#Set color
+#This script makes it easier to create a colored prompt script.
+
+#'non-printing escape sequences have to be enclosed in \[ESC[ and \].'
+NO_COLOR='\[\e[0m\]'  #\033=\e\=^[=\0x1b=ECH in ascii.
+_SC=NO_COLOR
+
+#Function to set color
 function SET_C() {
+    # If we get 0 parameters, then assume that we don't wish to change the color.
+    if [ $# = 0 ]; then
+        return
+    fi
     # If we only get one parameter, then assume it's just the color. Call this function with "plain" as the style
     if [ $# = 1 ]; then
         SET_C plain $1
@@ -9,55 +19,48 @@ function SET_C() {
     case $1 in
         "plain")        STYLE='00';;
         "light")        STYLE='01';;
-        "dark")            STYLE='02';;
-        "italic")        STYLE='03';;
+        "dark")         STYLE='02';;
+        "italic")       STYLE='03';;
         "underline")    STYLE='04';;
-        "reverse")        STYLE='07';;
-        *) _SC='\033[0m'; echo "error in style: '$1'"; return;;
+        "reverse")      STYLE='07';;
+        *)              _SC=NO_COLOR; echo "error in style: '$1'"; return;;
     esac
 
     case $2 in
         "BLACK")        COLOR="30";;
-        "RED")            COLOR="31";;
+        "RED")          COLOR="31";;
         "GREEN")        COLOR="32";;
-        "YELLOW")        COLOR="33";;
-        "BLUE")            COLOR="34";;
-        "PURPLE")        COLOR="35";;
-        "CYAN")            COLOR="36";;
-           "WHITE")        COLOR="37";;
-        *) _SC='\033[0m';  echo "error in color: '$2'"; return;;
+        "YELLOW")       COLOR="33";;
+        "BLUE")         COLOR="34";;
+        "PURPLE")       COLOR="35";;
+        "CYAN")         COLOR="36";;
+        "WHITE")        COLOR="37";;
+        *)              _SC=NO_COLOR; echo "error in color: '$2'"; return;;
     esac
     # Start color variable
-    _SC="\[\033[${STYLE};${COLOR}m\]"
+    _SC="\[\e[${STYLE};${COLOR}m\]"
+    #Fun fact: we have to escape the brackets to make sure that bash knows how to calculate PS1's length.
+    #Without the escaped brackets, bash would miscalculate PS1's length and wrap around at the wrong time.
 }
-#End color variable
-_EC="\[\e[0m\]"
+
+#This function makes my styling calls prettier.
+#Usage: STYLE <RESULT> <'string'> [[<style>] <color>]
+function STYLE() {
+  SET_C $3 $4
+  eval "$1='${_SC}$2'" #This establishes $1 as a shell variable.
+}
 
 ################################
 # Set the colors and parts of the prompt.
-
-SET_C dark YELLOW
-PS1_TIME="$_SC[\@]$_EC"
-
-SET_C underline RED
-PS1_USER="${_SC}\u$_EC"
-
-SET_C light RED
-PS1_HOST="${_SC}\h$_EC"
-
-SET_C light BLUE
-PS1_MIDDLE="${_SC}@$_EC"
-
-SET_C dark GREEN
-PS1_DIR="$_SC\w$_EC"
-
-SET_C light BLUE
-PS1_END="$_SC> $_EC"
-
-SET_C dark GREEN
-PS1_OPENB="$_SC[$_EC"
-PS1_CLOSEB="$_SC]$_EC"
+#Check out https://www.cyberciti.biz/tips/howto-linux-unix-bash-shell-setup-prompt.html as a reference for special PS1 chars
+STYLE PS1_TIME '[\@]' dark YELLOW
+STYLE PS1_USER '\u' underline RED
+STYLE PS1_AT '@' light BLUE
+STYLE PS1_HOST '\h' light RED
+STYLE PS1_DIR '\W' dark GREEN
+STYLE PS1_END '> ' light BLUE
+STYLE OPENB '[' dark GREEN; STYLE CLOSEB
 
 ################################
 # Do the actual export, placing the parts in any order
-export PS1="${PS1_OPENB}${PS1_USER}${PS1_MIDDLE}${PS1_HOST}${PS1_CLOSEB} $PS1_DIR $PS1_END"
+export PS1="${PS1_OPENB}${PS1_USER}${PS1_AT}${PS1_HOST}${PS1_CLOSEB} $PS1_DIR $PS1_END"
